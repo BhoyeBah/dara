a501f959709ca34011b090892f33c3b5e13d5948
Mamadou Bhoye Bah <bhoyemad11@gmail.com>
2025-04-29 11:07:34 +0000

    homme

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'da1d322b47c315a764da9559505e22bb4a1a4f88',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'da1d322b47c315a764da9559505e22bb4a1a4f88',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
